a0870b4f9f36493191a2e055d35ed65d79072b33
littlestone <ccmt.supersu@gmail.com>
2017-02-22 15:43:01 +0800

    add quickstart layout

---
+++ library.js
@@ -0,0 +1,32 @@
+"use strict";
+var controllers = require('./lib/controllers'),
+
+	plugin = {};
+
+plugin.init = function(params, callback) {
+	var router = params.router,
+		hostMiddleware = params.middleware,
+		hostControllers = params.controllers;
+
+	// We create two routes for every view. One API call, and the actual route itself.
+	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
+
+	router.get('/admin/plugins/ccmtpostinfo', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
+	router.get('/api/admin/plugins/ccmtpostinfo', controllers.renderAdminPage);
+	// router.get('/api/plugins/ccmtpostinfo/categories', controllers.categoriesPage);
+	router.get('/api/plugins/ccmtpostinfo', controllers.renderPage);
+
+	callback();
+};
+
+plugin.addAdminNavigation = function(header, callback) {
+	header.plugins.push({
+		route: '/plugins/ccmtpostinfo',
+		icon: 'fa-tint',
+		name: 'ccmtpostinfo'
+	});
+
+	callback(null, header);
+};
+
+module.exports = plugin;
---
+++ static/lib/admin.js
@@ -0,0 +1,27 @@
+'use strict';
+/* globals $, app, socket */
+
+define('admin/plugins/ccmtpostinfo', ['settings'], function(Settings) {
+
+	var ACP = {};
+
+	ACP.init = function() {
+		Settings.load('ccmtpostinfo', $('.ccmtpostinfo-settings'));
+
+		$('#save').on('click', function() {
+			Settings.save('ccmtpostinfo', $('.ccmtpostinfo-settings'), function() {
+				app.alert({
+					type: 'success',
+					alert_id: 'ccmtpostinfo-saved',
+					title: 'Settings Saved',
+					message: 'Please reload your NodeBB to apply these settings',
+					clickfn: function() {
+						socket.emit('admin.reload');
+					}
+				});
+			});
+		});
+	};
+
+	return ACP;
+});
