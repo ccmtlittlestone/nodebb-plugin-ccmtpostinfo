d151944bec8b6d654c6f91fa1e3de6eeb9e18bf3
littlestone <ccmt.supersu@gmail.com>
2017-02-23 16:11:50 +0800

    ready

--- library.js
+++ library.js
@@ -11,22 +11,9 @@ plugin.init = function(params, callback) {
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
 
-	router.get('/admin/plugins/ccmtpostinfo', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/ccmtpostinfo', controllers.renderAdminPage);
-	router.get('/api/plugins/ccmtpostinfo/categories', controllers.categoriesPage);
-	router.get('/api/plugins/ccmtpostinfo', controllers.renderPage);
+	router.get('/api/plugins/ccmtpostinfo/getpost/:pid', controllers.getPost);
 
 	callback();
-};
-
-plugin.addAdminNavigation = function(header, callback) {
-	header.plugins.push({
-		route: '/plugins/ccmtpostinfo',
-		icon: 'fa-tint',
-		name: 'ccmtpostinfo'
-	});
-
-	callback(null, header);
 };
 
 module.exports = plugin;
