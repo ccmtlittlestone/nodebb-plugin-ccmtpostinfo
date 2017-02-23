5d7a4eb3230bf70602387d97ac735a27d93e8082
phyer@sina.com <phyer@sina.com>
2017-02-24 03:15:47 +0800

    update policy

--- library.js
+++ library.js
@@ -10,10 +10,13 @@ plugin.init = function(params, callback) {
 
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
-
+	var allowOrigin=["http://localhost:4567","http://www.supersu.com","http://su.phyer.click"];
 	router.all('/*', function(req, res, next) {
 		console.log("origin:",req.headers.origin);
-		res.header("Access-Control-Allow-Origin", "*");
+		if(!allowOrigin.includes(req.headers.origin)){
+			return;
+		}
+		res.header("Access-Control-Allow-Origin", req.headers.origin);
 		res.header("Access-Control-Allow-Headers", "X-Requested-With");
 		next();
 	});
